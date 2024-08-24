91fcb46644914a848578a46fc7e8e791b107283d
Julien Dubost <julien.dubost@planny.ch>
2024-08-24 12:54:03 +0200

    feat: add whole game EV compute method

--- blackjack/ev.py
+++ blackjack/ev.py
@@ -6,8 +6,8 @@ def compute_game_ev():
     """
     Compute the whole game expected value
     """
-    # compute ev against each bank card
-    player_abs_ev = 0  # total absolute EV for the player
+    # compute expected value against each bank card
+    player_abs_val = 0  # total absolute value for the player
     for bank_card in constants.BANK_STARTING_CARDS:
         bank_card_probability = (
             1 / 13 if bank_card != constants.HandState.FIGURE else 4 / 13
@@ -19,30 +19,38 @@ def compute_game_ev():
             if best_move in [
                 constants.MOVE_STAND,
                 constants.MOVE_HIT,
-                constants.MOVE_SPLIT,
             ]:
-                # TODO: consider split EV
-                player_abs_ev += (
+                player_abs_val += (
                     probability
                     * bank_card_probability
-                    * (player_graph.max_evs[player_hand])
+                    * (player_graph.max_evs[player_hand] - 1)
                 )
             elif best_move in [
                 constants.MOVE_DOUBLE,
                 constants.MOVE_DOUBLE_ELSE_HIT,
                 constants.MOVE_DOUBLE_ELSE_STAND,
             ]:
-                player_abs_ev += (
+                player_abs_val += (
                     2
                     * bank_card_probability
                     * probability
-                    * (player_graph.hit_evs[player_hand])
+                    * (player_graph.hit_evs[player_hand] - 1)
                 )
             elif best_move in [
                 constants.MOVE_SURRENDER_ELSE_STAND,
                 constants.MOVE_SURRENDER_ELSE_HIT,
                 constants.MOVE_SURRENDER_ELSE_SPLIT,
             ]:
-                player_abs_ev += bank_card_probability * 0.5
+                player_abs_val += -bank_card_probability * probability * 0.5
+            elif best_move == constants.MOVE_SPLIT:
+                post_split_state = constants.POST_SPLIT_STATE[player_hand]
+                player_abs_val += (
+                    2
+                    * bank_card_probability
+                    * probability
+                    * (player_graph.max_evs[post_split_state] - 1)
+                )
+            else:
+                raise ValueError(f"Unknown move {best_move}")
 
-    return player_abs_ev
+    return player_abs_val
